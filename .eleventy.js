68d42b4c3deed284c144f2d38d2c381a5144f1e1
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-04-19 12:53:59 +0200

    feat: favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function configure(eleventyConfig) {
   eleventyConfig.addGlobalData('layout', 'base');
-  eleventyConfig.addPassthroughCopy({ 'src/style.css': 'style.css' });
+  eleventyConfig.addPassthroughCopy({
+    'src/public': '.',
+    'src/style.css': 'style.css',
+  });
   eleventyConfig.addFilter('prettyDate', (dateStr) => {
     const date = new Date(dateStr);
     return date.toDateString();
