164d09feb7336d61d38d3927cc2a1af5201ebed8
Rommert Zijlstra <rommert.thomas@gmail.com>
2024-05-03 12:10:01 +0200

    feat: updates footnotes, styling
    - use `markdown-it-footnotes`
    - use `markdown-it-abbr`
    - structure CSS a bit
    - update some styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,18 @@
 import markdownIt from 'markdown-it';
 import markdownItAnchor from 'markdown-it-anchor';
 import Shiki from '@shikijs/markdown-it';
+import markdownItAbbr from 'markdown-it-abbr';
+import markdownItFootnotes from 'markdown-it-footnote';
 
 const markdown = markdownIt({
   html: true,
   linkify: true,
   typographer: true,
 })
+  .use(markdownItAbbr)
   .use(markdownItAnchor)
-  .use(
-    await Shiki({ theme: 'houston' })
-  );
-
-const footnoteCache = new Map();
+  .use(markdownItFootnotes)
+  .use(await Shiki({ theme: 'houston' }));
 
 export default function configure(config) {
   config.setLibrary('md', markdown);
@@ -29,14 +29,6 @@ export default function configure(config) {
   config.addFilter('prettyDate', (dateStr) => {
     const date = new Date(dateStr);
     return date.toDateString();
-  });
-
-  config.addPairedShortcode('footnote', function (content) {
-    if (!footnoteCache.has(this.page)) footnoteCache.set(this.page, []);
-    const footnotes = footnoteCache.get(this.page);
-    const index1 = footnotes.push(markdown.renderInline(content));
-    this.page.footnotes = footnotes;
-    return `<sup class="footnote"><a id="notesrc${index1}" href="#note${index1}">${index1}</a></sup>`;
   });
 
   config.addFilter('json', (input) => JSON.stringify(input));
