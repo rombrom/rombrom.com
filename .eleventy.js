80280efd03f2f88e5bba90f9aa3c73ba612da530
Rommert Zijlstra <rommert.thomas@gmail.com>
2025-03-24 09:30:24 +0100

    feat: CV v1

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
-import markdownIt from 'markdown-it';
-import markdownItAnchor from 'markdown-it-anchor';
-import Shiki from '@shikijs/markdown-it';
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+import Shiki from "@shikijs/markdown-it";
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
-} from '@shikijs/transformers';
-import markdownItAbbr from 'markdown-it-abbr';
-import markdownItFootnotes from 'markdown-it-footnote';
+} from "@shikijs/transformers";
+import markdownItAbbr from "markdown-it-abbr";
+import markdownItFootnotes from "markdown-it-footnote";
 
 const markdown = markdownIt({
   html: true,
@@ -18,27 +18,24 @@ const markdown = markdownIt({
   .use(markdownItFootnotes)
   .use(
     await Shiki({
-      theme: 'houston',
+      theme: "houston",
       transformers: [transformerNotationDiff(), transformerNotationHighlight()],
-    })
+    }),
   );
 
 export default function configure(config) {
-  config.setLibrary('md', markdown);
+  config.setLibrary("md", markdown);
 
-  config.addGlobalData('date', 'git Created');
-  config.addGlobalData('layout', 'base');
-  config.addGlobalData('NODE_ENV', process.env.NODE_ENV);
+  config.addGlobalData("date", "git Created");
+  config.addGlobalData("layout", "base");
+  config.addGlobalData("NODE_ENV", process.env.NODE_ENV);
 
-  config.addPassthroughCopy({
-    'src/public': '.',
-    'src/style.css': 'style.css',
-  });
+  config.addPassthroughCopy({ "src/public": ".", "src/*.css": "css" });
 
-  config.addFilter('prettyDate', (dateStr) => {
+  config.addFilter("prettyDate", (dateStr) => {
     const date = new Date(dateStr);
     return date.toDateString();
   });
 
-  config.addFilter('json', (input) => JSON.stringify(input));
+  config.addFilter("json", (input) => JSON.stringify(input));
 }
